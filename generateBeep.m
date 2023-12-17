c9c6d6dbee55b64ffa61002822592d338918b000
Abdulrahman Sayed <fffds1456@gmail.com>
2023-12-18 01:44:45 +0200

    modified genrateBeep function for some user specifications

--- generateBeep.m
+++ generateBeep.m
@@ -1,12 +1,22 @@
 function generateBeep(~, ~)
-	Fs = 44100;
-    Ft = 2000;
+    Fs = 44100;
     
-	%Generating time vector
-	x = 0:1/Fs:1;
-
-	%Genarating Amplitude Vector
-	y = sin(2 * pi * Ft * x);
-	
+    % ask the user for the duration of the beep sound
+    beep_duration = inputdlg('Enter beep duration in seconds:', 'Beep Duration', [1 50]);
+    
+    if isempty(beep_duration)
+        return;  % Cancel button clicked, do nothing
+    end
+    
+    % Convert string input to a numeric value
+    beep_duration = str2num(beep_duration{1});
+    
+    % Generate time vector
+    x = 0:1/Fs:beep_duration;
+    
+    % Generate amplitude vector
+    y = sin(2 * pi * 2000 * x);
+    
+    % playing the beep sound
     sound(y, Fs);
 end
